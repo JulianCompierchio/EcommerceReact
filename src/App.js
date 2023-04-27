48e27670e2c9abdb24f9dab0426becfacf689027
Julian Compierchio <116223630+JulianCompierchio@users.noreply.github.com>
2023-04-27 11:54:16 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Header from './Components/Header.js'
+import Home from './Components/Home.js'
+import Footer from './Components/Footer.js'
+
+function App() {
+  return (
+  <>
+    <Header/>
+    <Home/>
+    <Footer/>    
+  </>    
+  );    
+}
+
+export default App;
