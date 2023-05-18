a9718c97048cd18c69535dd32804bd52be095101
NataliaPita <PitaFraile@gmail.com>
2023-05-18 16:36:48 -0300

    inicio de rama feature-patner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Components/Header/Header.js'
+import Header from './Components/Header/Header.js';
 import Home from './Components/Home/Home.js'
 import Footer from './Components/Footer/Footer.js'
+
+
+
+
 
 function App() {
   return (
   <>
     <Header/>
     <Home/>
-    <Footer/>    
+    <Footer/>
+         
   </>    
   );    
 }
--- src/Components/Features/Features.js
+++ src/Components/Features/Features.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import './Features.css';
+import FeatureItems from './FeatureItems';
+import FeatureImagen from '../Assets/cafe2.jpg';
+
+
+
+
+
+
+const featureTitulo = " Descubre el mundo del café.";
+const featureSubtitulo = "Nuestra misión es ofrecer a nuestros clientes una experiencia excepcional en el mundo del café, proporcionando productos de alta calidad y un servicio personalizado."
+
+
+
 
 const Features = () => {
+  
   return (
-    <div>Features</div>
+    <>
+    < section className='feature'>
+      <div>
+        <h3 className="feature-title">{featureTitulo}</h3>
+        <h4 className='feature-subt'>{featureSubtitulo}</h4>      
+          <FeatureItems />
+      </div> 
+      <div>
+         <img src = {FeatureImagen} alt='imagen-cafe' className='feature-imagen'/>
+      </div> 
+    </section>     
+    </>
   )
 }
 
