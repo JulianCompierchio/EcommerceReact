8feefd4d080a1c176d314e69fd5762aa44554a81
JulianCompierchio <julian_compierchio128@outlook.com>
2023-05-23 17:24:15 -0300

    Modificacion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals'
-import {ContadorProvider} from './Context/CounterContext'
+import reportWebVitals from './reportWebVitals';
+import {ContadorProvider} from './Context/CounterContext';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
