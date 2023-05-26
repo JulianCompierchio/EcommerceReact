5e5394a00a5ce2cf91ce6e51b6301be102350b8a
Serotonina <109625284+Serotonina12@users.noreply.github.com>
2023-05-26 17:49:57 -0300

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,61 +2,71 @@
 
 const Footer = () => {
   return (
-    <header>
       <div>
         <a href='https://lordicon.com/'>Icons by Lordicon.com</a>
       </div>   
-      <div className="container">
-        <div className="content">
-          <h2>Cuentale a un amigo</h2>
-            <div className="social-icons">
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
+     var MyClass = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <link rel="stylesheet" type="text/css" href="footer.css" />
+        <footer>
+          <div className="container">
+            <div className="content">
+              <h2>Cuentale a un amigo</h2>
+              <div className="social-icons">
+                <a href="#"><img src="images\logos\logo_FB.png" alt="Facebook" /></a>
+                <a href="#"><img src="images\logos\logo_IG.png" alt="Instagram" /></a>
+                <a href="#"><img src="images\logos\logo_TW.png" alt="Twitter" /></a>
+                <a href="#"><img src="images\logos\logo_WAp.png" alt="Whats App" /></a>
+                <a href="#"><img src="images\logos\logo-MS.png" alt="Messenger" /></a>
+                <a href="#"><img src="images\logos\logo-TT.png" alt="Tik-Tok" /></a>
+              </div>
             </div>
-        </div>
-      </div>
-      <div className="container-menu">
-        <div className="logo">
-        </div>
-        <nav className="menu">
-          <ul>
-            <li><a href="#">Ocasiones</a>
+          </div>
+          <div className="container-menu">
+            <div className="logo">
+              <img src="images\logos\logo.jpg" alt="Logo" />
+            </div>
+            <nav className="menu">
               <ul>
-                <li><a href="#">Trabajando desde casa</a></li>
-                <li><a href="#">Hora del snack</a></li>
-                <li><a href="#">Desayuno</a></li>
-                <li><a href="#">Vida vegana</a></li>
-                <li><a href="#">Horneado</a></li>
+                <li><a href="#">Ocasiones</a>
+                  <ul>
+                    <li><a href="#">Trabajando desde casa</a></li>
+                    <li><a href="#">Hora del snack</a></li>
+                    <li><a href="#">Desayuno</a></li>
+                    <li><a href="#">Vida vegana</a></li>
+                    <li><a href="#">Horneado</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Todas las categorías</a>
+                  <ul>
+                    <li><a href="#">Horneado</a></li>
+                    <li><a href="#">Desayuno</a></li>
+                    <li><a href="#">Condimentos</a></li>
+                    <li><a href="#">Bebidas</a></li>
+                    <li><a href="#">Saludables</a></li>
+                    <li><a href="#">Para golosos</a></li>
+                    <li><a href="#">Salsas</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Regalos</a>
+                  <ul>
+                    <li><a href="#">Caja de horneados</a></li>
+                    <li><a href="#">Sensaciones dulces</a></li>
+                    <li><a href="#">Sabores Locales</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Nuestras marcas</a></li>
+                <li><a href="#">Sobre nosotros</a></li>
               </ul>
-            </li>
-            <li><a href="#">Todas las categorías</a>
-                <ul>
-                  <li><a href="#">Horneado</a></li>
-                  <li><a href="#">Desayuno</a></li>
-                  <li><a href="#">Condimentos</a></li>
-                  <li><a href="#">Bebidas</a></li>
-                  <li><a href="#">Saludables</a></li>
-                  <li><a href="#">Para golosos</a></li>
-                  <li><a href="#">Salsas</a></li>
-                </ul>
-            </li>
-            <li><a href="#">Regalos</a>
-              <ul>
-                <li><a href="#">Caja de horneados</a></li>
-                <li><a href="#">Sensaciones dulces</a></li>
-                <li><a href="#">Sabores Locales</a></li>
-              </ul>
-            </li>
-              <li><a href="#">Nuestras marcas</a></li>
-              <li><a href="#">Sobre nosotros</a></li>
-          </ul>
-        </nav>
+            </nav>
+          </div>
+        </footer>
       </div>
-    </header>
+    );
+  }
+});
   )
 }
 
