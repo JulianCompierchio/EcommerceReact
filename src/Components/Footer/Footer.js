77210d8a0f06125550e404c53b385eb5d625262a
JulianCompierchio <julian_compierchio128@outlook.com>
2023-06-01 19:22:14 -0300

    Pequeños cambios

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,30 +5,24 @@ import './Footer.css'
 const Footer = () => {
   return (
     <header className ='container-fluid'>    
-      <div className="row contenedor-menu">
+      <div className="row contenedor-menu">    
         <div className="col-2">
           <img className='logo'src={logo} alt="Logo"/>
-        </div>
+        </div>   
         <nav className="col-10 d-inline-flex menu">
           <ul>
             <li><strong>Ocasiones</strong></li>
-            <li>Trabajando desde casa</li>
+            <li>Trabajo en casa</li>
             <li>Desayuno</li>
             <li>Vida vegana</li>
           </ul>
           <ul>
-            <li><strong>Todas las categorías</strong></li>
+            <li><strong>Categorías</strong></li>
             <li>Desayuno</li>
             <li>Saludables</li>
             <li>Para golosos</li>                  
           </ul>
-          <ul>
-            <li><strong>Regalos</strong></li>
-            <li>Cafeteras sensacionales</li>
-            <li>Sensaciones dulces</li>
-            <li>Sabores Locales</li>
-          </ul>
-              <li><strong>Nuestras marcas</strong></li>
+              <li><strong>Marcas</strong></li>
           <ul>
               <li><strong>Sobre nosotros</strong></li>
           </ul>
