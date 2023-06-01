de876e6fc74a96094ac0c21576b030947ae5371a
Joaquin Arena <joaquinarena2000@gmail.com>
2023-06-01 19:24:45 -0300

    subiendo componente

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+      <figcaption>
+
+      </figcaption>
+    </div>
   )
 }
 
---
+++ src/Components/AboutUs/AboutUsDB.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const cardsDB = [
+    {
+      id : 1,
+      destino : "villa angostura"
+      descripcion : "mongo"  
+    }, 
+    {
+        id : 2,
+        destino : "bariloche"
+        descripcion : "pepe"  
+      }, 
+      {
+        id : 3,
+        destino : "salta la linda "
+        descripcion : "peruanois "  
+      },  
+      {
+        id : 4,
+        destino : "españa "
+        descripcion : "ben10"  
+      }, 
+] 
